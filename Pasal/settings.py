3d99119b55466bb4030b0c8aa0931a86b1a27ef9
sanip <saneprijal@gmail.com>
2020-03-17 08:17:48 +0545

    change in settings for token authentication

--- Pasal/settings.py
+++ Pasal/settings.py
@@ -95,8 +95,9 @@ AUTH_PASSWORD_VALIDATORS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
+        # 'rest_framework.authentication.BasicAuthentication',
+        # 'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
     ]
 }
 
